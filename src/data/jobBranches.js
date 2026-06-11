84af8fe9e73830485bbc3552d88caf2e8331ae9f
SiddharthaAryan <131577270+SiddharthaAryan@users.noreply.github.com>
2026-06-12 00:18:30 +0530

    Add job branching data

---
+++ src/data/jobBranches.js
@@ -0,0 +1,5 @@
+export const departments=['Medical (Doctors & Consultants)','Nursing','Technicians & Diagnostics','Paramedical & Allied Health','Administrative & Management','Support & Facility Services','Other'];
+export const jobBranches={
+'Medical (Doctors & Consultants)':['RMO','Senior Medical Officer','Senior Resident','Junior Consultant','Associate Consultant','Consultant','Senior Consultant','Other'],
+'Nursing':['Staff Nurse','Senior Nurse','Incharge','ICU/OT Cath Nurse','Other'],
+'Technicians & Diagnostics':['Lab Technician','Radiology/X-Ray Tech','OT Technician','ECG/Echo Tech','Dial
